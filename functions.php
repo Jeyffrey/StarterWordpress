db8e9eadf23b3a15cc1942353629e6145129dfd4
geoffroy.begouaussel <gbegouaussel@nvos.fr>
2016-10-20 16:20:40 +0200

    Useless custom slugify function

--- functions.php
+++ functions.php
@@ -1,9 +1,11 @@
 <?php
+/* Some links : http://www.hongkiat.com/blog/useful-wordpress-functions/
+                https://github.com/taniarascia/wp-functions
+*/
 
 include get_template_directory() . '/functions-partials/' . 'init.wp.php';
 include get_template_directory() . '/functions-partials/' . 'init.enqueue.php';
 include get_template_directory() . '/functions-partials/' . 'init.medias.php';
-include get_template_directory() . '/functions-partials/' . 'functions.utils.php';
 include get_template_directory() . '/functions-partials/' . 'custom.post-types.php';
 include get_template_directory() . '/functions-partials/' . 'classes.pages.php';
 include get_template_directory() . '/functions-partials/' . 'backoffice.login.php';
